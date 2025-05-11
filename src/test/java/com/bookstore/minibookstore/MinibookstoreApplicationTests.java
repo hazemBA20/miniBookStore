0ec3da694db87d07a2a408837dd4013eb63c38de
hazemBA20 <hazeminsat@gmail.com>
2025-05-11 16:29:53 +0200

    first commit

---
+++ src/test/java/com/bookstore/minibookstore/MinibookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.minibookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
